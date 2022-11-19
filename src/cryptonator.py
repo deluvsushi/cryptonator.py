0937811a56b49e064dc5fbfa7366bb8f2e1b694e
ecclesias <77536370+zeviel@users.noreply.github.com>
2022-11-19 20:40:50 +0800

    Update cryptonator.py

--- src/cryptonator.py
+++ src/cryptonator.py
@@ -1,24 +1,23 @@
 from requests import get
 
-
 class Cryptonator:
-	def __init__(self):
+	def __init__(self) -> None:
 		self.api = "https://api.cryptonator.com/api"
 		self.headers = {
 			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
 		}
 
-	def get_currencies(self):
+	def get_currencies(self) -> dict:
 		return get(
 			f"{self.api}/currencies",
 			headers=self.headers).json()
 
-	def get_simple_ticker(self, currency_code: str):
+	def get_simple_ticker(self, currency_code: str) -> dict:
 		return get(
 			f"{self.api}/ticker/{currency_code}",
 			headers=self.headers).json()
 
-	def get_complete_ticker(self, currency_code: str):
+	def get_complete_ticker(self, currency_code: str) -> dict:
 		return get(
 			f"{self.api}/full/{currency_code}",
 			headers=self.headers).json()	
